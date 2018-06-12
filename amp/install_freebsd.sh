6954d42b5658491a12d0718e1056cb2229b2340f
Austin Selick-Bottos <segfault24@users.noreply.github.com>
2018-06-11 22:28:02 -0400

    switched to mariadb and made sql init functional

--- amp/install_freebsd.sh
+++ amp/install_freebsd.sh
@@ -18,7 +18,7 @@ echo Creating jail "${JAIL}" at ${IP}...
 cat <<__EOF__ >/tmp/pkg.json
 {
     "pkgs":[
-        "nano","bash","wget","curl","sudo","apache24","mod_php72","mysql57-server",
+        "nano","bash","wget","curl","sudo","apache24","mod_php72","mariadb102-server",
         "php72","php72-ctype","php72-curl","php72-dom","php72-filter","php72-hash",
         "php72-iconv","php72-json","php72-mbstring","php72-mysqli","php72-openssl",
         "php72-pdo","php72-pdo_mysql","php72-phar","php72-session","php72-tokenizer",
@@ -79,23 +79,19 @@ iocage exec ${JAIL} chmod 400 ${KEYOUT} ${CRTOUT}
 iocage exec ${JAIL} service apache24 start
 iocage exec ${JAIL} service mysql-server start
 
-# secure mysql installation
-TMP_DB_ROOT_PW=$(grep -v \# /root/.mysql_secret)
+# set the root db pass and save it
+iocage exec ${JAIL} mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}'";
 echo [mysql] > ${JAILROOT}/root/.my.cnf
-echo user=root >> ${JAILROOT}/root/.my.cnf
-echo password=${TMP_DB_ROOT_PW} >> ${JAILROOT}/root/.my.cnf
-iocage exec ${JAIL} mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}'";
-iocage exec ${JAIL} mysql -e "DELETE FROM mysql.user WHERE User='';"
-iocage exec ${JAIL} mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-iocage exec ${JAIL} mysql -e "DROP DATABASE IF EXISTS test;"
-iocage exec ${JAIL} mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
-iocage exec ${JAIL} mysqladmin reload
-
-# save the db password(s)
-echo [mysql] > ${JAILROOT}/root/.my.cnf
-echo user=root >> ${JAILROOT}/root/.my.cnf
 echo password=${DB_ROOT_PASSWORD} >> ${JAILROOT}/root/.my.cnf
 iocage exec ${JAIL} chmod 400 /root/.my.cnf
-iocage exec ${JAIL} rm /root/.mysql_secret
 echo See /root/.my.cnf for root password to mysql
+
+# secure mysql installation
+iocage exec ${JAIL} mysql -u root -e "DELETE FROM mysql.user WHERE User='';"
+iocage exec ${JAIL} mysql -u root -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+iocage exec ${JAIL} mysql -u root -e "DROP DATABASE IF EXISTS test;"
+iocage exec ${JAIL} mysql -u root -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
+
+# restart the whole jail to restart everything
+iocage restart ${JAIL}
 
