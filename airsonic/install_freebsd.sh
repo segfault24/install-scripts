4735aa128f6bb25e754ce0fd75f3e695bfec6c33
Austin Selick-Bottos <segfault24@users.noreply.github.com>
2019-01-28 23:48:26 -0500

    reverse proxy for https

--- airsonic/install_freebsd.sh
+++ airsonic/install_freebsd.sh
@@ -6,7 +6,7 @@ PROP="airsonic.properties"
 require_root
 require_file $PROP
 check_blank2 $PROP jail_name jail_fqdn jail_interface jail_ip jail_mask jail_gateway jail_vnet
-check_blank2 $PROP mount_src mount_dst mount_mode
+check_blank2 $PROP mount_src mount_dst mount_mode ext_ssl ext_fqdn
 
 JAIL_VERSION=11.2-RELEASE
 JAIL=$(prop $PROP jail_name)
@@ -21,6 +21,9 @@ DATASET=$(prop $PROP mount_src)
 DATADIR=$(prop $PROP mount_dst)
 DATAMODE=$(prop $PROP mount_mode)
 
+EXTSSL=$(prop $PROP ext_ssl)
+EXTFQDN=$(prop $PROP ext_fqdn)
+
 PASS=$(gen_passwd)
 
 # create the jail & install packages
@@ -29,7 +32,7 @@ install_pkg "tomcat8"
 
 # ffmpeg build & run dependencies
 install_pkg "nasm binutils frei0r gmake pkgconf libiconv perl5 fontconfig freetype2 gmp gnutls"
-install_pkg "libxcb opus libtheora libvorbis libvpx"
+install_pkg "libxcb opus libtheora libvorbis libvpx nginx"
 #sub dep?: texi2html cmake automake autoconf libtool xorg-macros
 init_ports
 config_port multimedia/ffmpeg
@@ -37,6 +40,7 @@ make_port multimedia/ffmpeg
 
 # set to start on boot
 iocage exec ${JAIL} sysrc tomcat8_enable="YES"
+iocage exec ${JAIL} sysrc nginx_enable="YES"
 
 # configure tomcat
 JAILROOT=/mnt/iocage/jails/${JAIL}/root
@@ -45,6 +49,26 @@ cp tomcat-users.xml tomcat-users.xml.tmp
 sed -i '' "s/ADMINPASSWORD/${PASS}/g" tomcat-users.xml.tmp
 install -m 400 -o www -g www tomcat-users.xml.tmp ${JAILROOT}/${TOMCAT}/conf/tomcat-users.xml
 rm tomcat-users.xml.tmp
+
+# configure nginx
+NGINX=/usr/local/etc/nginx
+cp nginx.conf nginx.conf.tmp
+sed -i '' "s/SERVERNAME/${EXTFQDN}/g" nginx.conf.tmp
+install -m 640 -o root -g wheel nginx.conf.tmp ${JAILROOT}/${NGINX}/nginx.conf
+rm nginx.conf.tmp
+
+#if [ $EXTSSL -eq "letsencrypt" ]
+#then
+#  iocage exec ${JAIL} "pkg install -y py27-certbot"
+#  iocage exec ${JAIL}
+#else
+  # generate self signed cert
+  SUBJ="/C=US/ST=New\ York/L=New\ York/O=The\ Ether/CN=${EXTFQDN}"
+  KEYOUT=${NGINX}/key.pem
+  CRTOUT=${NGINX}/cert.pem
+  iocage exec ${JAIL} "openssl req -x509 -nodes -days 1095 -newkey rsa:2048 -keyout ${KEYOUT} -out ${CRTOUT} -subj ${SUBJ}"
+  iocage exec ${JAIL} chmod 400 ${KEYOUT} ${CRTOUT}
+#fi
 
 # remove default tomcat deployments
 iocage exec ${JAIL} rm -rf ${TOMCAT}/webapps/ROOT
@@ -74,7 +98,9 @@ echo \!\!\!\!\!\!\!\!\!\!
 echo See /root/tomcat.password within the jail for the Tomcat admin password
 echo \!\!\!\!\!\!\!\!\!\!
 echo \!\!\!\!\!\!\!\!\!\!
-echo Go to http://${IP}:8080/airsonic and change the Airsonic admin password
+echo "Go change the Airsonic admin password"
+echo "      local: https://${IP}/airsonic"
+echo "   external: https://${FQDN}/airsonic"
 echo \!\!\!\!\!\!\!\!\!\!
 echo \!\!\!\!\!\!\!\!\!\!
 
